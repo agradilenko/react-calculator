5f08f1cc8fa3d6ef176d78068a9d678e2635fd57
agradilenko <44676263+agradilenko@users.noreply.github.com>
2020-06-22 13:10:35 +0300

    Updates

---
+++ precache-manifest.63a37c3bf41da2208f637e00b257d48c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbec45aa7006cf25e23735d755f8835",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "747fd68172e6cb46ab2f",
+    "url": "/react-calculator/static/css/main.b3fc2651.chunk.css"
+  },
+  {
+    "revision": "d152cb306bae5f320389",
+    "url": "/react-calculator/static/js/2.10b402a2.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator/static/js/2.10b402a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747fd68172e6cb46ab2f",
+    "url": "/react-calculator/static/js/main.0458d71e.chunk.js"
+  },
+  {
+    "revision": "4ad8d72ddf01a440dc4b",
+    "url": "/react-calculator/static/js/runtime-main.d3b7e15a.js"
+  }
+]);
